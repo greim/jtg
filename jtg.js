514de4577f53343bc63ec3d235d55bdb0e114397
Greg Reimer <gr@wayin.com>
2012-08-21 15:13:04 -0600

    added run links for code samples

--- jtg.js
+++ jtg.js
@@ -1,4 +1,26 @@
+/*
+Copyright (c) 2012 Greg Reimer
+http://obadger.com/
+"gregreimer" "at" "gmail" "dot" "com"
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 window.Turtle = function(canvas){
 
@@ -40,8 +62,17 @@ window.Turtle = function(canvas){
 	var q = (function(){	
 		var funs = [];
 		(function run(){
-			if (funs.length > 0) {
-				funs.shift()();
+			var len = funs.length;
+			if (len > 0) {
+				if (len > 500) {
+					// for really long runs, batch actions together
+					for (var i=0; i<len/250; i++) {
+						funs.shift()();
+					}
+				} else {
+					// otherwise one action at a time
+					funs.shift()();
+				}
 				setTimeout(run,0);
 			} else {
 				setTimeout(run, 200);
@@ -289,9 +320,19 @@ window.Turtle = function(canvas){
 				return heading * (180/Math.PI);
 			};
 		},
-		pen: function(){
+		pu: function(){
 			return function(){
-				return penDown ? 'down' : 'up';
+				return !penDown;
+			};
+		},
+		pd: function(){
+			return function(){
+				return penDown;
+			};
+		},
+		width: function(){
+			return function(){
+				return ctx.lineWidth;
 			};
 		},
 		fg: function(){
